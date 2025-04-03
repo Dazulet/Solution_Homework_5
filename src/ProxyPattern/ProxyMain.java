b6202f52aef7ccb9bc2709797149340f0157951a
Daulet <baltashdaulet3@gmail.com>
2025-04-03 15:53:44 +0500

    did logic in ProxyMain

--- src/ProxyPattern/ProxyMain.java
+++ src/ProxyPattern/ProxyMain.java
@@ -1,4 +1,14 @@
 package ProxyPattern;
 
 public class ProxyMain {
+    public static void main(String[] args) {
+        Image img1 = new ProtectedProxyImage("house1.jpg", true);
+        Image img2 = new ProtectedProxyImage("house2.jpg", false);
+
+        img1.displayThumbnail();
+        img2.displayThumbnail();
+
+        img1.displayFullImage();
+        img2.displayFullImage();
+    }
 }
