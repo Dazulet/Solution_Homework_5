dc30039472ed5c0841405faa1411c75159362f4f
Daulet <baltashdaulet3@gmail.com>
2025-04-04 11:12:31 +0500

    add 2 class and did logic in MarkerStyle,MarkerStyleFactory

--- src/FlyweightPattern/MarkerStyle.java
+++ src/FlyweightPattern/MarkerStyle.java
@@ -1,4 +1,17 @@
 package FlyweightPattern;
 
 public class MarkerStyle {
+    private final String iconType;
+    private final String color;
+    private final String labelStyle;
+
+    public MarkerStyle(String iconType, String color, String labelStyle) {
+        this.iconType = iconType;
+        this.color = color;
+        this.labelStyle = labelStyle;
+    }
+
+    public void display(int x, int y) {
+        System.out.println("Displaying " + iconType + " marker at (" + x + ", " + y + ") with color " + color);
+    }
 }
--- src/FlyweightPattern/MarkerStyleFactory.java
+++ src/FlyweightPattern/MarkerStyleFactory.java
@@ -1,4 +1,18 @@
 package FlyweightPattern;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MarkerStyleFactory {
+    private static final Map<String, MarkerStyle> styles = new HashMap<>();
+
+    public static MarkerStyle getMarkerStyle(String iconType, String color, String labelStyle) {
+        String key = iconType + "-" + color + "-" + labelStyle;
+        styles.putIfAbsent(key, new MarkerStyle(iconType, color, labelStyle));
+        return styles.get(key);
+    }
+
+    public static int getTotalStyles() {
+        return styles.size();
+    }
 }
