ee115e3b59beddd961dced0a726575222433d996
Daulet <baltashdaulet3@gmail.com>
2025-04-04 11:33:44 +0500

    FlyweightMain , MapMarker implement

--- src/FlyweightPattern/FlyweightMain.java
+++ src/FlyweightPattern/FlyweightMain.java
@@ -1,4 +1,19 @@
 package FlyweightPattern;
 
 public class FlyweightMain {
+    public static void main(String[] args) {
+        MapMarker[] markers = new MapMarker[10];
+
+        markers[0] = new MapMarker(10, 20, MarkerStyleFactory.getMarkerStyle("Hospital", "Red", "Bold"));
+        markers[1] = new MapMarker(15, 25, MarkerStyleFactory.getMarkerStyle("Hospital", "Red", "Bold"));
+        markers[2] = new MapMarker(30, 40, MarkerStyleFactory.getMarkerStyle("Restaurant", "Blue", "Italic"));
+        markers[3] = new MapMarker(50, 60, MarkerStyleFactory.getMarkerStyle("Gas Station", "Green", "Regular"));
+        markers[4] = new MapMarker(70, 80, MarkerStyleFactory.getMarkerStyle("Restaurant", "Blue", "Italic"));
+
+        for (MapMarker marker : markers) {
+            if (marker != null) marker.render();
+        }
+
+        System.out.println("Total unique marker styles created: " + MarkerStyleFactory.getTotalStyles());
+    }
 }
--- src/FlyweightPattern/MapMarker.java
+++ src/FlyweightPattern/MapMarker.java
@@ -1,4 +1,16 @@
 package FlyweightPattern;
 
 public class MapMarker {
+    private final int x, y;
+    private final MarkerStyle style;
+
+    public MapMarker(int x, int y, MarkerStyle style) {
+        this.x = x;
+        this.y = y;
+        this.style = style;
+    }
+
+    public void render() {
+        style.display(x, y);
+    }
 }
